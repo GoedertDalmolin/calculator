9ce34d70cf6ab1cda677d56dc4265b0413d1b916
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-02-05 16:26:21 -0300

    Created implementation for the components

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -2,6 +2,7 @@ import 'package:calculator/components/display.dart';
 import 'package:calculator/components/keyboard.dart';
 import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -21,6 +22,10 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       home: Column(
         children: [
